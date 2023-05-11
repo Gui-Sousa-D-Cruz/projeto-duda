64909f415e40192fb3ada60b5003fa22f9e53fcb
Guilherme Suusa da Cruz <gui.sdacruz6@users.noreply.github.com>
2023-05-11 13:59:43 -0300

    .

---
+++ script.js
@@ -0,0 +1,15 @@
+function entrar(){
+    let btn = window.document.querySelector('#btn')
+    btn.innerHTML='Sim'
+}
+
+function sair(){
+    let btn = window.document.querySelector('#btn')
+    btn.innerHTML='Não'
+}
+
+function clicar(){
+    let audio = window.document.querySelector('#audio')
+    audio.play();
+        
+}
